751f13567d6d4249fe3609f9429b52b3987bb8d4
martinprogrammer <martinprogrammer@outlook.com>
2014-11-29 22:26:26 +0000

    SOA Example

---
+++ EventTickets.Model/TicketPurchaseFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTickets.Model
+{
+    public class TicketPurchaseFactory
+    {
+        public static TicketPurchase CreateTicket(Event Event, int tktQty)
+        {
+            TicketPurchase ticket = new TicketPurchase();
+            ticket.Id = Guid.NewGuid();
+            ticket.TicketQuantity = tktQty;
+            ticket.Event = Event;
+
+            return ticket;
+        }
+    }
+}
---
+++ EventTickets.Model/TicketReservationFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTickets.Model
+{
+    public class TicketReservationFactory
+    {
+        public static TicketReservation CreateReservation(Event Event, int tktQty)
+        {
+            TicketReservation reservation = new TicketReservation();
+            reservation.Id = Guid.NewGuid();
+            reservation.TicketQuantity = tktQty;
+            reservation.Event = Event;
+            reservation.ExpiryTime = DateTime.Now.AddMinutes(1D);
+
+            return reservation;
+        }
+    }
+}
