d141cc0ca29157a978d6744252d0d7b3c481b302
martinprogrammer <martinprogrammer@outlook.com>
2014-12-09 15:47:25 +0000

    WCF added

---
+++ EventTickets.HttpHost/TicketService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EventTickets.HttpHost
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TicketService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TicketService.svc or TicketService.svc.cs at the Solution Explorer and start debugging.
+    public class TicketService : ITicketService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
